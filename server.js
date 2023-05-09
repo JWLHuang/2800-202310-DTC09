d7e97a5084d2d1618c41360e9b9872818fa8d558
Jacky Huang <whuang148@my.bcit.ca>
2023-05-09 11:46:25 -0700

    Add redirects to show sample pages

--- server.js
+++ server.js
@@ -6,3 +6,23 @@ const mongo = require('mongodb');
 const Joi = require('joi');
 let ejs = require('ejs');
 const bcrypt = require('bcrypt');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/404', (req, res) => {
+    res.render('404.ejs');
+});
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
