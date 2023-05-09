82e7c0dd899d22237ad8cca8e4e9f297ad432350
Jacky Huang <whuang148@my.bcit.ca>
2023-05-09 12:03:57 -0700

    Debugging host issue

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const bcrypt = require('bcrypt');
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.set("views", __dirname + "/views");
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
