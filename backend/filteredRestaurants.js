aab1545d903ac49ed0bd56a1b2971f60e144746e
eric-deau <edeau@my.bcit.ca>
2023-05-10 15:02:37 -0700

    Add filteredRestaurants.js to repo

---
+++ backend/filteredRestaurants.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    searchByEmail: (searchObject) => {
+        const result = authorizationModel.find(
+            { email: searchObject }
+        );
+        return result
+    }
+}
