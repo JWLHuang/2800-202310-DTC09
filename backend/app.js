255afce207b2cf0b2e4cda62ec463ce973139f53
eric-deau <edeau@my.bcit.ca>
2023-05-09 18:03:31 -0700

    Basic footer implemented with temporary content

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,26 @@ const mongo = require('mongodb');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const url = require('url');
+
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './frontend/views/');
 
+// importing static files for navigation links
+const footerLinks = require('../frontend/footerLinks');
+
+// middleware to set global variables for header and footer links
+app.use('/', (req, res, next) => {
+    res.locals.footerLinks = footerLinks.footerLinks;
+    res.locals.socialLinks = footerLinks.socialLinks;
+    next();
+});
+
 app.get('/', (req, res) => {
+    // console.log(req.url)
     res.render('index.ejs');
 });
 
@@ -20,6 +33,7 @@ const authorizationRoutes = require('./authorizationRoutes');
 const profileRoutes = require('./profileRoutes');
 const restaurantListRoutes = require('./restaurantListRoutes');
 const restPasswordRoutes = require('./restPasswordRoutes');
+
 
 app.use(signupRoutes);
 app.use(authorizationRoutes);
