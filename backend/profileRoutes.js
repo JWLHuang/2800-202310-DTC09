345618acc9c03a7c33deaf5447c6af9655940988
nathan-yau <nshyau@gmail.com>
2023-05-09 22:07:58 -0700

    Add Profile Edit route

--- backend/profileRoutes.js
+++ backend/profileRoutes.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { searchByEmail } = require('./searchProfile')
+const usersModel = require("./models/usersModel");
+const Joi = require("joi");
 
-router.get('/profile', (req, res) => {
-    res.render('profile.ejs');
+const schema = Joi.object({
+    about_me: Joi.string().min(1).max(100).trim().required(),
 });
+
+router.get('/profile', async (req, res) => {
+    profile = await searchByEmail(req.session.email)
+    console.log(profile[0].dietary_perferences)
+    res.render('profile.ejs', { 'profile': profile[0] });
+});
+
+router.post('/profileUpdate', async (req, res) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        console.log('About me field is invalid');
+    }
+    else {
+        usersModel.updateOne({ email: req.session.email }, { about_me: req.body.about_me }).then((result) => {
+            console.log('About me field updated');
+            res.redirect('/profile');
+        }).catch((err) => {
+            console.log('About me field not updated');
+        });
+    }
+})
+
 
 module.exports = router;
