d41da81c6957fd98e11c811cda09f25fa8ba49ee
Jacky Huang <whuang148@my.bcit.ca>
2023-05-09 20:09:07 -0700

    Rename file due to typo

--- backend/restPasswordRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/resetPassword', (req, res) => {
-    res.render('resetPassword.ejs');
-});
-
-module.exports = router;
