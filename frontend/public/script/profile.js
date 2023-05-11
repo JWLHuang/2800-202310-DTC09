ff24fbbdd387d29224710321e6615f33a75e6992
NATHAN YAU <nshyau@gmail.com>
2023-05-10 22:54:12 -0700

    Update Profile frontend js for modularization

--- frontend/public/script/profile.js
+++ frontend/public/script/profile.js
@@ -1,5 +1,4 @@
 function toggleTab(id) {
-
     $('.nav-link').removeClass('active')
     $('.tab-pane').removeClass('active')
     $(`.${id}`).addClass('active')
@@ -8,22 +7,37 @@ function toggleTab(id) {
 
 
 const setup = () => {
-    console.log("run")
+    $(`.navProfile`).addClass('active')
     $('body').on('click', '.tab', function () {
         toggleTab(this.id);
     })
-    $('body').on('click', '.edit-button', async function () {
-        $('#edit-modal').removeClass('d-none');
+    $('body').on('click', '.editnavProfile', async function () {
+        $('#editnavProfile').removeClass('d-none');
+        $('#backdrop').removeClass('d-none');
+        $('body').addClass('stop-scrolling')
+        // $('#shinyForm').prop('checked', false);
+    });
+    $('body').on('click', '.editnavPreferences', async function () {
+        $('#editnavPreferences').removeClass('d-none');
+        $('#backdrop').removeClass('d-none');
+        $('body').addClass('stop-scrolling')
+        // $('#shinyForm').prop('checked', false);
+    });
+    $('body').on('click', '.editnavFactors', async function () {
+        $('#editnavFactors').removeClass('d-none');
         $('#backdrop').removeClass('d-none');
         $('body').addClass('stop-scrolling')
         // $('#shinyForm').prop('checked', false);
     });
     $('body').on('click', '.close-modal', async function () {
-        $('#edit-modal').addClass('d-none');
+        $('#editnavProfile').addClass('d-none');
+        $('#editnavPreferences').addClass('d-none');
+        $('#editnavFactors').addClass('d-none');
         $('#backdrop').addClass('d-none');
         $('body').removeClass('stop-scrolling')
         // $('#shinyForm').prop('checked', false);
     });
+    $('.mobileSelect').mobileSelect();
 }
 
 $(document).ready(setup)
---
+++ frontend/public/script/profileLinks.js
@@ -0,0 +1,15 @@
+const navProfile = [
+    { id: 'email', heading: 'Registered Email' },
+    { id: 'about_me', heading: 'About me' },
+]
+
+const navPreferences = [
+    { id: 'dietary_preferences', heading: 'Dietary Preferences' },
+]
+
+const navFactors = [
+    { id: 'user_preferences', heading: 'User Preferences' }
+]
+
+
+module.exports = { navProfile, navPreferences, navFactors }
