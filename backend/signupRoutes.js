d417f5e954cf95764a8d1373a150f6654d9bcd8b
Derek Woo <me@derekw.co>
2023-05-11 14:10:06 -0700

    Add signup error handler to signupRoutes.js

--- backend/signupRoutes.js
+++ backend/signupRoutes.js
@@ -15,18 +15,21 @@ const signupSchema = Joi.object(
 
 router.get('/signup', async (req, res) => {
     const user = await findUser({ email: req.session.email });
-    res.render('signup.ejs', user ? { user: user } : { user: null });
+    user ? res.locals.user = user : res.locals.user = null;
+    res.render('signup.ejs');
 });
 
 router.use(express.urlencoded({ extended: false }))
 router.post("/signup", async (req, res) => {
     const validationResult = signupSchema.validate(req.body);
     if (validationResult.error != null) {
-        // res.render("authError", { errorType: "Signup", errorMsg: validationResult.error.details[0].message })
-        console.log(validationResult.error.details[0].message)
+        const user = await findUser({ email: req.session.email });
+        user ? res.locals.user = user : res.locals.user = null;
+        res.render('signup.ejs', { errorMsg: validationResult.error.details[0].message });
     } else if (await usersModel.findOne({ email: req.body.email, })) {
-        // res.render("authError", { errorType: "Signup", errorMsg: "Email already exists." })
-        console.log("Email already exists.")
+        const user = await findUser({ email: req.session.email });
+        user ? res.locals.user = user : res.locals.user = null;
+        res.render('signup.ejs', { errorMsg: "Email already exists" });
     } else {
         newUser = {
             email: req.body.email,
