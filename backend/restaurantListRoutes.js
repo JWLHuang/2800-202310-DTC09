dc52e25c471ab5e968f2a6ad55724ab5a61c267f
Derek Woo <me@derekw.co>
2023-05-11 13:21:15 -0700

    Add error handling to restaurantListRoutes.js

--- backend/restaurantListRoutes.js
+++ backend/restaurantListRoutes.js
@@ -4,18 +4,32 @@ const restaurantModel = require("./models/restaurantModel");
 const { findUser } = require("./findUser");
 
 router.get('/restaurants', async (req, res) => {
+    const errorMsg = req.session.error ? req.session.error : null;
+    delete req.session.error;
     const user = await findUser({ email: req.session.email });
     const restaurants = await restaurantModel.find({
         FacilitiesAndServices: /Vegetarian/i
     });
     console.log(restaurants)
-    res.render('restaurantList.ejs', restaurants ? { user: user, restaurants: restaurants } : { user: user, restaurants: null });
+    res.render('restaurantList.ejs', restaurants ? { user: user, restaurants: restaurants, errorMsg: errorMsg } : { user: user, restaurants: null, errorMsg: errorMsg });
 });
 
-router.get('/restaurant/:id', async (req, res) => {
-    const restaurant = await restaurantModel.findOne({ _id: req.params.id });
+router.get('/restaurant/:id?', async (req, res) => {
     const user = await findUser({ email: req.session.email });
-    res.render("restaurant.ejs", restaurant ? { user: user, restaurant: restaurant, userLatitude: 49.17555, userLongitude: -123.13254 } : { user: user, restaurant: null });
+    try {
+        const restaurant = await restaurantModel.findOne({ _id: req.params.id });
+        if (restaurant) {
+            res.render("restaurant", restaurant ? { user: user, restaurant: restaurant, userLatitude: 49.17555, userLongitude: -123.13254 } : { user: user, restaurant: null });
+        } else {
+            req.session.error = "No restaurant selected";
+            res.redirect("/restaurants")
+        }
+
+    } catch (error) {
+        req.session.error = "Restaurant not found";
+        res.redirect("/restaurants")
+
+    }
 });
 
 module.exports = router;
