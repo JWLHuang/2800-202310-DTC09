d23b606a4f82ee4ca4888633c12ed46928ed548d
eric-deau <edeau@my.bcit.ca>
2023-05-16 14:53:32 -0700

    Remove comments from aiFilter.js

--- backend/aiFilter.js
+++ backend/aiFilter.js
@@ -1,4 +1,3 @@
-const restaurantModel = require("./models/restaurantModel");
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -8,7 +7,6 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const findRestaurants = async (restaurantList) => {
-    // console.log('restaurantList', restaurantList)
     // const prompt = `Restaurant Recommendation\n\nPlease recommend me a random restaurant that must be from this following list:\n${JSON.stringify(restaurantList)}\nConsider the cuisine type, rating, and location while making recommendations. 1.`
     const prompt = `Restaurant Recommendation\n\nPlease recommend random restaurants for me from this following list:\n${JSON.stringify(restaurantList)}\nConsider the cuisine type, rating, and location while making recommendations. Give me up to 5 restaurants. 1. `;
     try {
@@ -19,39 +17,13 @@ const findRestaurants = async (restaurantList) => {
             temperature: 0.9,
             n: 5,
         });
-        // console.log('response', response.data.choices[0].text);
-        // const recommendations = response.data.choices.map(choice => choice.text.trim());
-        // const recommendations = response.data.choices.slice(0, Math.min(restaurantList.length, 5)).map((choice, index) => {
-        //     const restaurantIndex = index + 1;
-        //     return {
-        //         ...restaurantList[restaurantIndex - 1],
-        //         recommendation: choice.text.trim(),
-        //         rank: restaurantIndex
-        //     };
-        // });
         const recommendations = response.data.choices
             .slice(0, Math.min(restaurantList.length, 5))
             .map((choice, index) => restaurantList[index]);
-        // const recommendations = response.data.choices.slice(0, Math.min(restaurantList.length, 5)).map((choice, index) => {
-        //     restaurantList[index]
-        // });
-        // console.log('recommendations', recommendations)
         return recommendations;
-        // console.log('generatedFilter', generatedFilter)
     } catch (err) {
         console.log(err);
     }
-
-    // try {
-    //     const restaurants = await restaurantModel.find(searchQuery).exec();
-    //     const filteredRestaurantsAI = restaurants.filter((restaurant) =>
-    //         restaurant.name.toLowerCase().includes(generatedFilter.toLowerCase())
-    //     );
-    //     console.log('filteredRestaurantsAI', filteredRestaurantsAI)
-    //     return filteredRestaurantsAI
-    // } catch (err) {
-    //     console.log(err);
-    // }
 }
 
 module.exports = findRestaurants;
