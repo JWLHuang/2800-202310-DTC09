5ba0b075eb42206db55f939045e971ffd8b089ab
Derek Woo <me@derekw.co>
2023-05-24 14:01:17 -0700

    Add documentation to mapRoutes.js

--- backend/mapRoutes.js
+++ backend/mapRoutes.js
@@ -5,8 +5,9 @@ const { findUser } = require("./findUser");
 
 router.get('/map', async (req, res) => {
     if (req.session.authenticated) {
+        // Get list of restaurant objects from database
         var restaurants = await restaurantModel.find({ _id: { $ne: "646562b76644f1aa93bc2ba2" } }, { Name: 1, _id: 1, Longitude: 1, Latitude: 1, Award: 1, Cuisine: 1 });
-        // const user = await findUser({ email: req.session.email });
+        // Render map with restaurants
         return res.render("map", { user: req.session, restaurants: JSON.stringify(restaurants) });
     } else {
         return res.redirect("/login");
