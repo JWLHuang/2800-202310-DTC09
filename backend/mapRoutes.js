d49f1feb6d6444a6ecacdb07d81f1305adc1368a
NATHAN YAU <nshyau@gmail.com>
2023-05-24 00:53:28 -0700

    Fix navbar display error on map page

--- backend/mapRoutes.js
+++ backend/mapRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const restaurantModel = require("./models/restaurantModel");
+const { findUser } = require("./findUser");
+
 router.get('/map', async (req, res) => {
     if (req.session.authenticated) {
         var restaurants = await restaurantModel.find({ _id: { $ne: "646562b76644f1aa93bc2ba2" } }, { Name: 1, _id: 1, Longitude: 1, Latitude: 1, Award: 1, Cuisine: 1 });
-        return res.render("map", { restaurants: JSON.stringify(restaurants) });
+        const user = await findUser({ email: req.session.email });
+        return res.render("map", { user: user, restaurants: JSON.stringify(restaurants) });
     } else {
         return res.redirect("/login");
     }
