8cedc86f87e7564149bf3a10a50f1c2a57576ae3
eric-deau <edeau@my.bcit.ca>
2023-05-24 10:31:48 -0700

    Remove search query from mapRoutes.js

--- backend/mapRoutes.js
+++ backend/mapRoutes.js
@@ -6,8 +6,8 @@ const { findUser } = require("./findUser");
 router.get('/map', async (req, res) => {
     if (req.session.authenticated) {
         var restaurants = await restaurantModel.find({ _id: { $ne: "646562b76644f1aa93bc2ba2" } }, { Name: 1, _id: 1, Longitude: 1, Latitude: 1, Award: 1, Cuisine: 1 });
-        const user = await findUser({ email: req.session.email });
-        return res.render("map", { user: user, restaurants: JSON.stringify(restaurants) });
+        // const user = await findUser({ email: req.session.email });
+        return res.render("map", { user: req.session, restaurants: JSON.stringify(restaurants) });
     } else {
         return res.redirect("/login");
     }
