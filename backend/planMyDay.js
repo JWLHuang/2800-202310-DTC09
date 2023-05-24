f8e0c1d235b9204f17bbc718aa71656dadab593f
Derek Woo <me@derekw.co>
2023-05-24 14:01:29 -0700

    Add documentation to planMyDay.js

--- backend/planMyDay.js
+++ backend/planMyDay.js
@@ -10,8 +10,7 @@ module.exports = {
     getTopThree: async (restaurantList) => {
         const prompt = `Please only return a JSON object of exactly 3 restaurants for each of Breakfast, Lunch, and Dinner from this list (only one per meal), in the following format: {"Breakfast":"645bdf114c5057693bcac787","Lunch":"645bdf114c5057693bcac78d","Dinner":"645bdf114c5057693bcac78f"}. \nList:\n${JSON.stringify(restaurantList)}\nConsider the cuisine type, rating, and hours. Please MAKE SURE they are in the same country. If none meet the criteria, please use the value "646562b76644f1aa93bc2ba2". Please return nothing else.`;
 
-        // console.log(prompt)
-
+        //  Send prompt to OpenAI API and return the response.
         try {
             const response = await openai.createCompletion({
                 model: 'text-davinci-003',
